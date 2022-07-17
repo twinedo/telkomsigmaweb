a0e120548f4ab7344ff2e32eecf33e274e2efb62
twinedo <twinedo.dev@gmail.com>
2022-07-18 02:45:36 +0700

    usefetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import "./App.scss";
 import { Spacer } from "./components";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useFetch from "./services/useFetch";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("tab1");
 
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/damaryosaaji/freeapi/main/user.json"
-      );
-
-      const data = await response.json();
-      console.log("data", data);
-      setData(data);
-    };
-
-    fetchData();
-  }, []);
+  const { data, error, loading } = useFetch(
+    "https://raw.githubusercontent.com/damaryosaaji/freeapi/main/user.json"
+  );
 
   const randomRGB = () => Math.floor(Math.random() * 255);
 
@@ -42,80 +31,93 @@ function App() {
               Settings
             </div>
           </div>
-          {selectedTab === "tab1" && (
-            <div className="tab1-container">
-              <div className="tab1-section">
-                <Spacer height={20} />
-                <div
-                  style={{ display: "flex", flex: 1, flexDirection: "column" }}
-                >
-                  <div className="justify-center">
-                    <img
-                      src="https://randomuser.me/api/portraits/lego/7.jpg"
-                      className="avatar"
-                      alt="profile-pict"
-                    />
-                  </div>
-                  <Spacer height={50} />
-                  <div className="title-section">Username</div>
-                  <div className="value-section">{data?.username}</div>
-                  <Spacer height={10} />
-                  <div className="title-section">Fullname</div>
-                  <div className="value-section">{data?.fullname}</div>
-                </div>
-                <div style={{ position: "relative" }}>
-                  <div
-                    className="btn-outlined"
-                    style={{ position: "absolute", right: 0, bottom: 0 }}
-                  >
-                    Edit
-                  </div>
-                </div>
+          {selectedTab === "tab1" &&
+            (loading ? (
+              <div className="coming-soon">
+                <h3>Loading...</h3>
               </div>
-              <div className="divider-line" />
-              <div className="tab2-section">
-                <h3>Applications</h3>
-                <Spacer height={15} />
-                <div className="list">
-                  {data?.aplikasi?.map((val) => (
-                    <div key={val?.id_aplikasi}>
-                      <div className="card-item">
-                        <div>
-                          <img
-                            src={`https://picsum.photos/id/${val?.id_aplikasi}/300/200`}
-                            alt="random"
-                            style={{ borderRadius: 5 }}
-                          />
-                        </div>
-                        <div>{val?.nama_aplikasi}</div>
-                        <Spacer height={20} />
-                        <div className="role-list">
-                          {val?.roles?.map((o, i) => (
-                            <div key={o.id_role} style={{ margin: 4 }}>
-                              <div
-                                className="role-item"
-                                style={{
-                                  backgroundColor: `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`,
-                                }}
-                              >
-                                {o.nama_role}
+            ) : error ? (
+              <div className="coming-soon">
+                <h3>Oops.. Error with fetching data</h3>
+              </div>
+            ) : (
+              <div className="tab1-container">
+                <div className="tab1-section">
+                  <Spacer height={20} />
+                  <div
+                    style={{
+                      display: "flex",
+                      flex: 1,
+                      flexDirection: "column",
+                    }}
+                  >
+                    <div className="justify-center">
+                      <img
+                        src="https://randomuser.me/api/portraits/lego/7.jpg"
+                        className="avatar"
+                        alt="profile-pict"
+                      />
+                    </div>
+                    <Spacer height={50} />
+                    <div className="title-section">Username</div>
+                    <div className="value-section">{data?.username}</div>
+                    <Spacer height={10} />
+                    <div className="title-section">Fullname</div>
+                    <div className="value-section">{data?.fullname}</div>
+                  </div>
+                  <div style={{ position: "relative" }}>
+                    <div
+                      className="btn-outlined"
+                      style={{ position: "absolute", right: 0, bottom: 0 }}
+                    >
+                      Edit
+                    </div>
+                  </div>
+                </div>
+                <div className="divider-line" />
+                <div className="tab2-section">
+                  <h3>Applications</h3>
+                  <Spacer height={15} />
+                  <div className="list">
+                    {data?.aplikasi?.map((val) => (
+                      <div key={val?.id_aplikasi}>
+                        <div className="card-item">
+                          <div>
+                            <img
+                              src={`https://picsum.photos/id/${val?.id_aplikasi}/300/200`}
+                              alt="random"
+                              style={{ borderRadius: 5 }}
+                            />
+                          </div>
+                          <div>{val?.nama_aplikasi}</div>
+                          <Spacer height={20} />
+                          <div className="role-list">
+                            {val?.roles?.map((o, i) => (
+                              <div key={o.id_role} style={{ margin: 4 }}>
+                                <div
+                                  className="role-item"
+                                  style={{
+                                    backgroundColor: `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`,
+                                  }}
+                                >
+                                  {o.nama_role}
+                                </div>
+                                {i < val?.roles?.length - 1 && (
+                                  <Spacer width={10} />
+                                )}
                               </div>
-                              {i < val?.roles?.length - 1 && (
-                                <Spacer width={10} />
-                              )}
-                            </div>
-                          ))}
+                            ))}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
 
-                  <Spacer width={15} />
+                    <Spacer width={15} />
+                  </div>
+                  <Spacer height={25} />
                 </div>
-                <Spacer height={25} />
               </div>
-            </div>
-          )}
+            ))}
           {selectedTab === "tab2" && (
             <div className="coming-soon">
               <img
---
+++ src/services/useFetch/index.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const dataResponse = await response.json();
+        setData(dataResponse);
+      } catch (err) {
+        console.log("error", err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
